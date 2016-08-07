34a02dcfd2e9b0a27e669cea01d6ddcb1c7bfd9b
snitkdan <snitkdan@uw.edu>
2016-08-06 20:44:07 -0700

    Cleaned up views with shortcuts

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music' #name-spacing the urls!
+
 urlpatterns = [
     #/music/
     url(r'^$', views.index, name = 'index'),
-
     #/music/id e.g. /music/180
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
 
