d0e8e33fa233209088b0cc0d115e267f8b61eed1
snitkdan <snitkdan@uw.edu>
2016-08-06 17:04:09 -0700

    Updated models with "Album" and "Song"

--- music/models.py
+++ music/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+#Ex: the album 'Red' has pk '1'
 
-# Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE) #whenever we delete an album, any songs that were linked are deleted as well.
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
